7af73145f03aeebabde02cf075e862d2de5330b8
cunguyendev <cunguyen.dev@gmail.com>
2020-12-30 11:22:18 +0700

    Handle generate the code on input the text.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,13 @@
 import QRCode from 'qrcode';
+import image from '../assets/images/placeholder-image.jpg';
 
 window.addEventListener('DOMContentLoaded', () => {
   const year = document.querySelector('.main-footer__copyright').innerHTML.replace('$year', `${new Date().getFullYear()}`);
 
   document.querySelector('.main-footer__copyright').innerHTML = year;
+  const textInput = document.getElementById('yourtext');
 
-  document.querySelector('.btn-create').addEventListener('click', () => {
-    const textInput = document.getElementById('yourtext');
+  textInput.addEventListener('keyup', () => {
     const opts = {
       errorCorrectionLevel: 'H',
       type: 'image/jpeg',
@@ -23,6 +24,8 @@ window.addEventListener('DOMContentLoaded', () => {
         if (err) throw err;
         document.getElementById('qr-code-display').src = url;
       });
+    } else {
+      document.getElementById('qr-code-display').src = image;
     }
   });
 });
